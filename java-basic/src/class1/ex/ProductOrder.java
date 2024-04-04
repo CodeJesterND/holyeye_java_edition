bf2f6a49dcd22a6a10784e1c145786497a355888
devhyoseung <dev.hyoseung@gmail.com>
2024-04-04 21:31:11 +0900

    today study upload

--- java-basic/src/class1/ex/ProductOrder.java
+++ java-basic/src/class1/ex/ProductOrder.java
@@ -1,7 +1,12 @@
 package class1.ex;
 
 public class ProductOrder {
+    // 상품명
     String productName;
+
+    // 가격
     int price;
+
+    // 주문 수량
     int quantity;
 }
--- java-basic/src/class1/ex/ProductOrderMain.java
+++ java-basic/src/class1/ex/ProductOrderMain.java
@@ -1,34 +1,36 @@
 package class1.ex;
 
 public class ProductOrderMain {
-
     public static void main(String[] args) {
-        ProductOrder[] orders = new ProductOrder[3];
+        // 여러 상품의 주문 정보를 담는 배열 생성
+        // 상품 주문 정보를 'ProductOrder' 타입의 변수로 받아 저장
+        // 상품 주문 정보와 최종 금액 출력
+
+        ProductOrder[] productOrders = new ProductOrder[3];
 
         ProductOrder order1 = new ProductOrder();
         order1.productName = "두부";
         order1.price = 2000;
         order1.quantity = 2;
-        orders[0] = order1;
+        productOrders[0] = order1;
 
         ProductOrder order2 = new ProductOrder();
         order2.productName = "김치";
         order2.price = 5000;
         order2.quantity = 1;
-        orders[1] = order2;
+        productOrders[1] = order2;
 
         ProductOrder order3 = new ProductOrder();
         order3.productName = "콜라";
         order3.price = 1500;
         order3.quantity = 2;
-        orders[2] = order3;
+        productOrders[2] = order3;
 
         int totalAmount = 0;
-        for (ProductOrder order : orders) {
-            System.out.println("상품명: " + order.productName + ", 가격: " + order.price + ", 수량: " + order.quantity);
-            totalAmount += order.price * order.quantity;
+        for (ProductOrder productOrder : productOrders) {
+            System.out.println("상품명: " + productOrder.productName + ", 가격: " + productOrder.price + "원, 주문 수량: " + productOrder.quantity);
+            totalAmount += productOrder.price * productOrder.quantity;
         }
-
         System.out.println("총 결제 금액: " + totalAmount);
     }
 }
